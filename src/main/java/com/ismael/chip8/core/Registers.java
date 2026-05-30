07bd90faa0f8e9b21b6ce9d0799ac75848248b40
mael <maelribeiro59@gmail.com>
2026-05-30 10:28:36 -0400

    feat(core): implement cpu registers and program counter

---
+++ src/main/java/com/ismael/chip8/core/Registers.java
@@ -0,0 +1,71 @@
+package com.ismael.chip8.core;
+
+public class Registers {
+
+		private final int[] v;
+		private int i;
+		private int pc;
+		private int sp;
+		private int delayTimer;
+		private int soundTimer;
+		
+		public Registers() {
+			this.v = new int[16];
+			this.pc = 0x200;
+		}
+
+		public int getI() {
+			return i;
+		}
+
+		public void setI(int i) {
+			this.i = i & 0xFFF;
+		}
+
+		public int getPc() {
+			return pc;
+		}
+
+		public void setPc(int pc) {
+			this.pc = pc & 0xFFFF;
+		}
+		
+		public void incrementPc() {
+			this.pc = (this.pc + 2) & 0xFFFF;
+		}
+
+		public int getSp() {
+			return sp;
+		}
+
+		public void setSp(int sp) {
+			this.sp = sp & 0xFFFF;
+		}
+
+		public int getDelayTimer() {
+			return delayTimer;
+		}
+
+		public void setDelayTimer(int delayTimer) {
+			this.delayTimer = delayTimer & 0xFFFF;
+		}
+
+		public int getSoundTimer() {
+			return soundTimer;
+		}
+
+		public void setSoundTimer(int soundTimer) {
+			this.soundTimer = soundTimer & 0xFFF;
+		}
+
+		public int getV(int index) {
+			return v[index];
+		}
+		
+		public void setV(int index, int value) {
+			this.v[index] = value & 0xFF;
+		}
+		
+		
+		
+}
