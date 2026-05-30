1d597bd1bed5c2f6ac5d454f8eefa07dd6c87106
mael <maelribeiro59@gmail.com>
2026-05-30 11:39:54 -0400

    feat(cpu): add decode logic to slice opencodes

--- src/main/java/com/ismael/chip8/core/CPU.java
+++ src/main/java/com/ismael/chip8/core/CPU.java
@@ -6,6 +6,13 @@ public class CPU {
 	private final Registers registers;
 	private final Stack stack;
 	
+	private int opcode;
+	private int nnn;
+	private int n;
+	private int x;
+	private int y;
+	private int kk;
+	
 	public CPU(Memory memory, Registers registers, Stack stack) {
 		super();
 		this.memory = memory;
@@ -18,5 +25,18 @@ public class CPU {
 		
 		int byte1 = memory.read(pc);
 		int byte2 = memory.read(pc + 1);
+		
+		registers.incrementPc();
+		
+		return byte1 << 8 | byte2;
+	}
+	
+	public void decode(int opcode) {
+		this.opcode = opcode;
+		this.nnn = opcode & 0x0FFF;
+		this.n = opcode & 0x000F;
+		this.x = (opcode & 0x0F00) >> 8;
+		this.y = (opcode & 0x00F0) >> 4;
+		this.kk = opcode & 0x00FF;
 	}
 }
